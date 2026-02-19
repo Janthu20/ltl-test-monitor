bcb06a300e8eab93de8225d005cf74024106e5f2
Isuru Wijerathna <xpisuru@gmail.com>
2026-02-20 02:46:14 +0530

    Fix base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/',   // ⭐ IMPORTANT FIX
+    base: '/ltl-test-monitor/',   // ⭐ IMPORTANT FIX
 
     server: {
       port: 3000,
